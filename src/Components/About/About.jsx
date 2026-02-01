65245d90e9965994653250cf23c8445849538b68
Saloni_Thete <Saloni662001@gmail.com>
2026-02-01 11:53:04 +0530

    portfolio updated

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -18,8 +18,9 @@ const About = () => {
         </div> */}
         <div className="about-right">
             <div className="about-para">
-                <p>A passionate developer with a strong focus on frontend development and mobile app solutions. I have contributed to open-source projects using version control systems like Git and GitHub, which has helped me collaborate effectively and grow as a developer.</p>
-                <p>In addition to my technical skills, I have a creative side, with a love for music and travel, which fuels my attention to detail and problem-solving abilities. I thrive in dynamic environments and am always eager to learn new technologies and enhance my development capabilities.</p>
+                <p>I am a dedicated developer with a strong focus on frontend architecture and mobile app solutions. Beyond core development, I am Salesforce Certified, allowing me to bridge the gap between custom user interfaces and powerful CRM ecosystems.</p>
+                <p>My experience spans contributing to open-source projects via Git and GitHub, where I’ve honed my ability to collaborate in high-velocity teams. Driven by a creative background in music and travel, I bring a unique perspective to problem-solving and a meticulous eye for detail. </p>
+                <p>In addition to my technical skills, I have a creative side, with a love for music and travel, which fuels my attention to detail and problem-solving abilities. I thrive in dynamic environments and am constantly seeking to leverage new technologies to build seamless, user-centric experiences.</p>
             </div>
             <div className="about-skills">
                 <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"70%"}}/></div>
@@ -34,7 +35,7 @@ const About = () => {
       </div>
       <div className="about-achievements">
         <div className="about-achievement">
-            <h1>1+</h1>
+            <h1>2+</h1>
             <p>YEARS OF EXPERIENCE</p>
         </div>
         <hr />
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
         <img src={profile_img} alt="" />
-        <h1><span>I'm Saloni Thete ,</span> fullstack developer.</h1>
+        <h1><span>I'm Saloni Thete ,</span> Software developer.</h1>
         {/* <p>I am a fullstack developer</p> */}
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
